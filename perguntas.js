b7cf9384199f66d49bb73f5ca32c871b80b3fa38
IsaacIndio <00001087885449sp@al.educacao.sp.gov.br>
2024-11-26 15:57:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Astronomia',
+    'Qual o Palneta mais proximos do sol?',
+    'O Planeta Mercurio'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual estado fica a cidade de São Pauio?',
+    'São Paulo'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual o Palneta conhecido por ser azul?',
+    'A Terra, Urano e Netuno'
+)
+
+criaCartao(
+    'ingles',
+    'What color is the orange fruit??',
+    'Orange'
+)
